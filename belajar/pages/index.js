c0e74bf693e360ec95ac097fbe778608317a5d04
alvinwidi33 <alvin.widi@uiac.id>
2024-10-03 17:40:20 +0700

    tugas-day9-done

--- belajar/pages/index.js
+++ belajar/pages/index.js
@@ -1,6 +1,13 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Main({ children }) {
+  useEffect(() => {
+    fetch("/api/hello")
+    .then(res => res.json())
+    .then((res)=> console.log('response =>',res))
+    .catch((err) => console.log('err',err))
+  })
   return (
     <div>
     <Layout metaTitle="Home">
